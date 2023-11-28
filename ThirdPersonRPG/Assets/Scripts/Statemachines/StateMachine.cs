ff8d9d80b6d251431a698568643dc4f3a6dce740
Ryan Hinds <115953645+FossRyanH@users.noreply.github.com>
2023-11-27 22:34:36 -0700

    init commit

---
+++ ThirdPersonRPG/Assets/Scripts/Statemachines/StateMachine.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    State _currentState;
+
+    void Update()
+    {
+        if (_currentState != null)
+        {
+            _currentState.Tick(Time.deltaTime);
+        }
+    }
+
+    public void SwitchState(State newState)
+    {
+        if (_currentState != null)
+        {
+            _currentState.ExitState();
+            _currentState = newState;
+            _currentState.EnterState();
+        }
+    }
+}
